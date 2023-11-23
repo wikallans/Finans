70dfa2566e6b7292a549579902ff55b9f46306ad
D4vidS1lva <132412220+D4vidS1lva@users.noreply.github.com>
2023-11-23 18:52:23 -0300

    começando a fazer formulario pelo php

--- cadastro.php
+++ cadastro.php
@@ -47,6 +47,43 @@
         </div>
       </nav>
     </header>
+    <?php
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Configurações para a conexão ao PostgreSQL (substitua com suas configurações)
+    $host = "localhost";
+    $port = "5432";
+    $dbname = "nome_do_banco";
+    $user = "usuario";
+    $password = "senha";
+    
+    // Conecta ao banco de dados PostgreSQL
+    $conexao = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+    // Verifica a conexão
+    if (!$conexao) {
+        die("Conexão falhou: " . pg_last_error());
+    }
+
+    // Obtém dados do formulário
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT); // Recomendado: armazenar senhas criptografadas
+
+    // Insere dados no banco de dados
+    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+    $resultado = pg_query($conexao, $sql);
+
+    if ($resultado) {
+        echo "Cadastro realizado com sucesso!";
+    } else {
+        echo "Erro no cadastro: " . pg_last_error($conexao);
+    }
+
+    // Fecha a conexão
+    pg_close($conexao);
+}
+?>
     <div class="container">
         <h2 class="mt-4">Cadastro de Cliente</h2>
     
@@ -143,5 +180,43 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
  
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
 </body>
 </html>
